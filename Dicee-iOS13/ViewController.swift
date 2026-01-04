13b0d4bca4d886538fb4bdf5313ea22cd22e257e
Ahmed Omar Sakib <omarsakib99@gmail.com>
2026-01-05 10:50:47 +1100

    Implement dice roll functionality in ViewController
    Added outlets for dice images and implemented roll functionality.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Use ctrl to use the pointer to create diceImageView1 and 2
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    //Creatimg variables
+//    var leftDiceNumber = 1
+//    var rightDiceNumber = 5
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    // Use #imageLiteral( to put the images in
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // use let to create a constant
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        diceImageView1.image = diceArray.randomElement( )
+        diceImageView2.image = diceArray[ Int.random(in: 1...5)]
+        
+//        leftDiceNumber = leftDiceNumber+1
+//        rightDiceNumber = rightDiceNumber-1
+        
+       
+        
+            }
+    
 }
+
 
